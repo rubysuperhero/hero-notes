570d819107c7fde924fc07a2003e1b86ec1d58f7
Joshua Toyota <jearsh@gmail.com>
2015-03-26 21:48:21 -0400

    autocommitting lib/clitasks/project_file.rb

--- lib/clitasks/project_file.rb
+++ lib/clitasks/project_file.rb
@@ -2,45 +2,68 @@ module CliTasks
   class ProjectFile
     class << self
       def for_dir(path=Dir.pwd, options={})
-        original_dir = Dir.pwd
-        Dir.chdir(world.path)
+        new(path, options)
       end
+    end
 
-      def collect_file_list(dir='')
-        pattern = dir.sub(/\/+$/, ?/) + ?*
-        files = []
-        dirs = []
-        Dir[pattern].each do |f|
-          next if File.basename(f)[/^[.]/]
-          if File.directory?(f)
-            f = f.sub(/\/*$/, ?/)
-            dirs += ['', f] + collect_file_list(f)
-          else
-            files << f
-          end
+    attr_accessor :original_dir, :path, :options
+
+    def initialize(path=Dir.pwd, options={})
+      @original_dir = Dir.pwd
+      @path = path
+      @options = options
+      Dir.chdir(world.path)
+    end
+
+    def generate
+
+    end
+
+    def generate_and_open
+
+    end
+
+    def file_list(dir='')
+      pattern = dir.sub(/\/+$/, ?/) + ?*
+      files = []
+      dirs = []
+      Dir[pattern].each do |f|
+        next if File.basename(f)[/^[.]/]
+        if File.directory?(f)
+          f = f.sub(/\/*$/, ?/)
+          dirs += ['', f] + file_list(f)
+        else
+          files << f
         end
-        files + dirs
       end
+      files + dirs
+    end
 
-      def file
-        'file-index'
-      end
+    def file_data
+      data = file_list
+      data = data.unshift(Dir.pwd, '')
+      data = data.join("\n")
+      data.gsub(/^/, '> ')
+    end
 
-      def save_file
-        IO.write(file, list.unshift(Dir.pwd, '').join("\n").gsub(/^/, '> '))
-      end
+    def filename
+      'project-index'
+    end
 
-      def open_file
-        system("vim", file)
-      end
+    def absolute_path
+      File.join(path, filename)
+    end
 
-      def index
-        list = collect_file_list
-      end
+    def save_file
+      IO.write(filename, file_data)
+    end
 
-      def world
-        @world ||= World.instance
-      end
+    def open_file
+      system("vim", filename)
+    end
+
+    def world
+      @world ||= World.instance
     end
   end
 end
